2feb8831f92a7e0b6f76bf9a8c8c6e8a82cfee4b
unknown <hudson.israel716@gmail.com>
2021-11-03 09:02:41 -0300

    Update filer

--- Commands_DML.sql
+++ Commands_DML.sql
@@ -1,26 +1,37 @@
-/*
-DML - Data Manipulation Language - Linguagem de manipulação de dados, são comandos utilizados para interagir com os dados dentro das tabelas, por exemplo os comandos: UPDATE, DELETE, INSERT.
 
-A instrução SQL UPDATE  // The SQL UPDATE Statement
-A UPDATE instrução é usada para modificar os registros existentes em uma tabela. // The UPDATE statement is used to modify the existing records in a table.
-*/
+-- DML - Data Manipulation Language - Linguagem de manipulação de dados, são comandos utilizados para interagir com os dados dentro das tabelas, por exemplo os comandos: UPDATE, DELETE, INSERT.
+
+The SQL UPDATE Statement
+-- A UPDATE instrução é usada para modificar os registros existentes em uma tabela.
+-- The UPDATE statement is used to modify the existing records in a table.
 
 --UPDATE Syntax
 UPDATE table_name
 SET column1 = value1, column2 = value2, ...
 WHERE condition;
---EX 1
+
+--  A instrução SQL a seguir atualiza o primeiro cliente (CustomerID = 1) com uma nova pessoa de contato e uma nova cidade.
+-- The following SQL statement updates the first customer (CustomerID = 1) with a new contact person and a new city.
+-- Example 1 
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
+-- Example 2
 UPDATE Person.Person
 SET FirstName = 'Alfred Schmidt', LastName = 'Frankfurt'
 WHERE BusinessEntityID = 5;
 
-/*
-ATUALIZAR vários registros // UPDATE Multiple Records
-É a WHERE cláusula que determina quantos registros serão atualizados. // It is the WHERE clause that determines how many records will be updated.
-A seguinte instrução SQL atualizará o ContactName para "Juan" para todos os registros em que country seja "Mexico": // The following SQL statement will update the ContactName to "Juan" for all records where country is "Mexico":
-*/
 
---EX
+-- ATUALIZAR vários registros
+-- UPDATE Multiple Records
+
+-- É a WHERE cláusula que determina quantos registros serão atualizados.
+-- It is the WHERE clause that determines how many records will be updated.
+
+-- A seguinte instrução SQL atualizará o ContactName para "Juan" para todos os registros em que country seja "Mexico":
+-- The following SQL statement will update the ContactName to "Juan" for all records where country is "Mexico":
+
+-- Example 2
 UPDATE Customers
 SET ContactName='Juan'
 WHERE Country='Mexico';
@@ -31,39 +42,64 @@ WHERE Country='Mexico';
 UPDATE Customers
 SET ContactName='Juan';
 
-/*
-A instrução SQL DELETE // The SQL DELETE Statement
-A DELETE instrução é usada para excluir registros existentes em uma tabela. // The DELETE statement is used to delete existing records in a table.
-*/
+The SQL DELETE Statement
 
---Sintaxe DELETE
+-- The DELETE statement is used to delete existing records in a table.
+DELETE Syntax
 DELETE FROM table_name WHERE condition;
---EX
+
+-- Example 1
+-- A seguinte instrução SQL exclui o cliente "Alfreds Futterkiste" da tabela "Clientes":
+-- The following SQL statement deletes the customer "Alfreds Futterkiste" from the "Customers" table:
 DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
 
---Apagar todos os registros // Delete All Records
---É possível excluir todas as linhas de uma tabela sem excluir a tabela. Isso significa que a estrutura, os atributos e os índices da tabela ficarão intactos: //It is possible to delete all rows in a table without deleting the table. This means that the table structure, attributes, and indexes will be intact:
-
+-- Delete All Records
+-- É possível excluir todas as linhas de uma tabela sem excluir a tabela. Isso significa que a estrutura, os atributos e os índices da tabela ficarão intactos:
+-- It is possible to delete all rows in a table without deleting the table. This means that the table structure, attributes, and indexes will be intact:
 DELETE FROM table_name;
 
-/*
-A instrução SQL INSERT INTO // The SQL INSERT INTO Statement
+-- Example 2
+-- A seguinte instrução SQL exclui todas as linhas da tabela "Clientes", sem excluir a tabela:
+-- The following SQL statement deletes all rows in the "Customers" table, without deleting the table:
+DELETE FROM Customers;
 
-A INSERT INTOinstrução é usada para inserir novos registros em uma tabela. // The INSERT INTO statement is used to insert new records in a table.
-Sintaxe INSERT INTO
-É possível escrever a INSERT INTO declaração de duas maneiras: // It is possible to write the INSERT INTO statement in two ways:
-*/
+The SQL INSERT INTO SELECT Statement
 
--- 1. Especifique os nomes das colunas e os valores a serem inseridos:
--- 1. Specify both the column names and the values to be inserted:
-INSERT INTO table_name (column1, column2, column3, ...)
-VALUES (value1, value2, value3, ...);
+-- A INSERT INTO SELECT instrução copia dados de uma tabela e os insere em outra tabela.
+-- The INSERT INTO SELECT statement copies data from one table and inserts it into another table.
 
---EX
+-- A INSERT INTO SELECT instrução requer que os tipos de dados nas tabelas de origem e destino correspondam.
+-- The INSERT INTO SELECT statement requires that the data types in source and target tables matches.
+
+INSERT INTO SELECT
+-- Copie todas as colunas de uma tabela para outra:
+-- Copy all columns from one table to another table:
+INSERT INTO table2
+SELECT * FROM table1
+WHERE condition;
+
+-- Copie apenas algumas colunas de uma tabela para outra:
+-- Copy only some columns from one table into another table:
+INSERT INTO table2 (column1, column2, column3, ...)
+SELECT column1, column2, column3, ...
+FROM table1
+WHERE condition;
+
+-- A seguinte instrução SQL copia "Fornecedores" em "Clientes" (as colunas que não são preenchidas com dados conterão NULL):
+-- The following SQL statement copies "Suppliers" into "Customers" (the columns that are not filled with data, will contain NULL):
+-- Example 1
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers;
+
+-- A seguinte instrução SQL copia "Fornecedores" em "Clientes" (preencha todas as colunas):
+-- The following SQL statement copies "Suppliers" into "Customers" (fill all columns):
+-- Example 2
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
-VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+SELECT SupplierName, ContactName, Address, City, PostalCode, Country FROM Suppliers;
 
--- 2. Se você estiver adicionando valores para todas as colunas da tabela, não será necessário especificar os nomes das colunas na consulta SQL. No entanto, certifique-se de que a ordem dos valores esteja na mesma ordem das colunas da tabela. Aqui, a INSERT INTOsintaxe seria a seguinte:
--- 2. If you are adding values for all the columns of the table, you do not need to specify the column names in the SQL query. However, make sure the order of the values is in the same order as the columns in the table. Here, the INSERT INTO syntax would be as follows:
-INSERT INTO table_name
-VALUES (value1, value2, value3, ...);
+-- A seguinte instrução SQL copia apenas os fornecedores alemães em "Clientes":
+-- The following SQL statement copies only the German suppliers into "Customers":
+-- Example 3
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers
+WHERE Country='Germany';
